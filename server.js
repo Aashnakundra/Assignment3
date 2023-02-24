d28355582764e871ee905dc7e95614b9f4d4eeac
Aashnakundra <aashnakundra7@gmail.com>
2023-02-24 16:18:41 -0500

    commit9

--- server.js
+++ server.js
@@ -45,92 +45,92 @@ function About(req,res){
     res.sendFile(path.join(__dirname,"/views/about.html"));
 };
       
-app.get("/blog", Blogs)
-function Blogs(req,res){
-    filemy.PublishedPosts().then((pst) => res.send(pst))
-    .catch((err) => {
-      console.log(err);
-    });
-}
+// app.get("/blog", Blogs)
+// function Blogs(req,res){
+//     filemy.PublishedPosts().then((pst) => res.send(pst))
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
 
-app.get("/posts",Posts)
-function Posts(req,res)
-{
-    let givCat =req.query.category;
-    let givDt = req.query.minDate;
-    if (givCat) {
-        filemy.PostsByCategory.then((res) =>res.send(res)).catch((error) =>
-        console.error(error)
-        );
-    } else if(givDt) {
-        PostsMinDate(givDt)
-        .then((res) => res.send(res))
-        .catch((error) =>console.error(error));
-    } else {
-        filemy
-        .AllPosts()
-        .then((postt) => res.send(postt))
-        .catch((err) => {
-            console.log(err);
-        });
-    }
-}
+// app.get("/posts",Posts)
+// function Posts(req,res)
+// {
+//     let givCat =req.query.category;
+//     let givDt = req.query.minDate;
+//     if (givCat) {
+//         filemy.PostsByCategory.then((res) =>res.send(res)).catch((error) =>
+//         console.error(error)
+//         );
+//     } else if(givDt) {
+//         PostsMinDate(givDt)
+//         .then((res) => res.send(res))
+//         .catch((error) =>console.error(error));
+//     } else {
+//         filemy
+//         .AllPosts()
+//         .then((postt) => res.send(postt))
+//         .catch((err) => {
+//             console.log(err);
+//         });
+//     }
+// }
         
-app.get("/categories",Categories)
-function Categories(req,res)
-{
-    filemy.AllCategories().then ((categories) => res.send(categories))
-    .catch((err) => {
-        console.log(err);
-    });
-}
+// app.get("/categories",Categories)
+// function Categories(req,res)
+// {
+//     filemy.AllCategories().then ((categories) => res.send(categories))
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// }
 
 
-app.get('/posts/add', upload.single("photo"), (req, res) => {
-    if(req.file) {
-        let streamUpload = (req) => {
-            return new Promise((resolve, reject) => {
-                let stream = cloudinary.uploader.upload_stream(
-                    (error, result) => {
-                        if (result) {
-                            resolve(result);
-                        } else {
-                            reject(error);
-                        }
-                    }
-                );
+// app.get('/posts/add', upload.single("photo"), (req, res) => {
+//     if(req.file) {
+//         let streamUpload = (req) => {
+//             return new Promise((resolve, reject) => {
+//                 let stream = cloudinary.uploader.upload_stream(
+//                     (error, result) => {
+//                         if (result) {
+//                             resolve(result);
+//                         } else {
+//                             reject(error);
+//                         }
+//                     }
+//                 );
     
-                streamifier.createReadStream(req.file.buffer).pipe(stream);
-            });
-        };
-        async function upload(req) {
-            let result = await streamUpload(req);
-            console.log(result);
-            return result;
-        }
+//                 streamifier.createReadStream(req.file.buffer).pipe(stream);
+//             });
+//         };
+//         async function upload(req) {
+//             let result = await streamUpload(req);
+//             console.log(result);
+//             return result;
+//         }
     
-        upload(req).then((uploaded)=>{
-            processPost(uploaded.url);
-        });
-    }else{
-        processPost("");
-    }
-    function processPost(imageUrl){
-        req.body.photo = imageUrl;
+//         upload(req).then((uploaded)=>{
+//             processPost(uploaded.url);
+//         });
+//     }else{
+//         processPost("");
+//     }
+//     function processPost(imageUrl){
+//         req.body.photo = imageUrl;
 
-        filemy.addPost(req.body).then(() => res.redirect("/posts"));
-    }
-});
-app.get("/posts/add",(req,res) => {
-    res.sendFile(path.join(___dirname, "/views/addPost.html"));
-});
-app.get("/posts/:id",(req,res) => {
-    let givId = req.params.id;
-    filemy
-    .PostsID(givId)
-    .then((res) => res.send(res))
-    .catch((error) => console.error(error));
-});
+//         filemy.addPost(req.body).then(() => res.redirect("/posts"));
+//     }
+// });
+// app.get("/posts/add",(req,res) => {
+//     res.sendFile(path.join(___dirname, "/views/addPost.html"));
+// });
+// app.get("/posts/:id",(req,res) => {
+//     let givId = req.params.id;
+//     filemy
+//     .PostsID(givId)
+//     .then((res) => res.send(res))
+//     .catch((error) => console.error(error));
+// });
     
 filemy.start().then((res) =>
 {
